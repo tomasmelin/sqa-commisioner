9a987e1d5f10c75954b4288aed46cde64e818077
tomasmelin <tomas.k.melin@gmail.com>
2015-04-14 19:05:31 +0800

    updated gitignore and added files for sale detail

---
+++ classes/SaleDetail.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tomas
+ * Date: 2015-04-14
+ * Time: 18:55
+ */
+
+class SalesDetail {
+
+}
---
+++ views/sale_detail.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tomas
+ * Date: 2015-04-14
+ * Time: 18:56
+ */
